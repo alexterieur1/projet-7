686c8b25e053a2c167ebab8474cf281a3fffaefc
Alexandrew <alexandrerichard45@sfr.fr>
2022-12-29 11:10:57 +0100

    rectification du retour erreur api

--- src/api.js
+++ src/api.js
@@ -1,5 +1,4 @@
 import logements from './logements.json'
-import Erreur from './pages/Erreur'
 
 export const getAllLogement = async () => {
     return logements
@@ -10,6 +9,6 @@ export const getOneLogement = async (id) => {
     if(logementData){
         return logementData
     }else{
-        return <Erreur/>
+        throw new Error("logement introuvable")
     }
 }
